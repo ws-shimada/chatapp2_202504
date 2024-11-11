039c36df13325f73795219f89b88854a6b08c3c5
ws-shimada <w9j9i24zt@gmail.com>
2024-11-11 13:49:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def input_id():
     with st.form("id_form", enter_to_submit=False):
         option = st.selectbox(
             "プロンプト選択※テスト用フォーム",
-            ("prompt_list[0]", "prompt_list[1]"),)
+            ("{}".format(prompt_list[0]), "{}".format(prompt_list[1])),)
         user_id = st.text_input('idを入力してください')
         submit_id = st.form_submit_button(
             label="送信",

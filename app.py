481e001935baaa1c65b364bad4cfa2cb2771ba29
ws-shimada <w9j9i24zt@gmail.com>
2024-11-14 18:28:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -177,8 +177,9 @@ def chat_page():
         # st.write("total output tokens : {}※テスト用".format(st.session_state.total_output_tokens))
         st.markdown(
             f"""
-            会話が規定回数に達しました。以下の"アンケートに戻る"をクリックして、アンケートに回答してください。
-            アンケートページは別のタブで開きます。
+            会話が規定回数に達しました。\n\n
+            以下の"アンケートに戻る"をクリックして、アンケートに回答してください。\n\n
+            アンケートページは別のタブで開きます。\n\n
             <a href="{url}" target="_blank">アンケートに戻る</a>
             """,
             unsafe_allow_html=True)
